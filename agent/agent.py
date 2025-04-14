e477a6a515f06c61e4577645db267d8879215671
Matias Bustos SM <matias.bustos.sm@outlook.com>
2025-04-13 20:17:24 -0400

    bugs in error reporting fixed

--- agent/agent.py
+++ agent/agent.py
@@ -287,7 +287,12 @@ class DPMAgent:
                 proc_info["state"] = STATE_FAILED
                 proc_info["exit_code"] = proc.poll()
                 proc_info["proc"] = None
-                #proc_info["errors"] = "Process stopped unexpectedly."
+                #check if roc_info["stdout_lines"] and proc_info["stderr_lines"] are not None
+                if proc_info["stdout_lines"] is not None and proc_info["stderr_lines"] is not None:
+                    proc_info["errors"]   = "".join(proc_info["stdout_lines"])
+                    proc_info[["errors"] += "".join(proc_info["stderr_lines"])
+                else:
+                    proc_info["errors"] = "Process stopped unexpectedly."
 
                 if proc_info["restart"]:
                     logging.info(f"Monitor Process: Restarting process {process_name}.")
